94d74597df9b208744cb094d6b58c7c9a8de9a43
kenchy <paulkench@yahoo.co.uk>
2011-01-24 15:56:27 +0000

    Incremental changes to towards end-point

--- 3viewdump.pl
+++ 3viewdump.pl
@@ -5,9 +5,9 @@ use Net::UPnP::AV::MediaServer;
 
 use Shell qw(curl ffmpeg);
 use Data::Dumper;
-use Date::Format;
-use Date::Parse;
-use Date::Manip;
+#use Date::Format;
+#use Date::Parse;
+#use Date::Manip;
 #use Date::Calc;
 #use HTTP::Date qw(parse_date time2str); 
 
@@ -18,10 +18,10 @@ use Date::Manip;
 # program info
 #------------------------------
 
-$program_name = 'DLNA Media Sever 2 Vodcast';
-$copy_right = 'Copyright (c) 2005 Satoshi Konno';
-$script_name = 'dms2vodcast.pl';
-$script_version = '1.0.3';
+$program_name = '3viewdump';
+$copy_right = 'parts Copyright (c) 2005 Satoshi Konno';
+$script_name = '3viewdump.pl';
+$script_version = '0.1';
 
 #------------------------------
 # global variables
@@ -33,29 +33,24 @@ $script_version = '1.0.3';
 # command option
 #------------------------------
 
-$rss_file_name = "";
 $base_directory = "./";
-$rss_base_url= "http://localhost";
-$rss_description = "CyberGarage Vodcast";
-$rss_language = "";
+$rss_description = "RSS of 3view Programs";
 $rss_link= "";
-$rss_title = "CyberGarage";
+$rss_title = "3 view RSS";
 $requested_count = 0;
-$mp4_format = 'ipod';
+$rss_file = "";
 $title_regexp = "";
 $search_date = "";
  
 @command_opt = (
 ['-a', '--date', 'date string', 'The date string to parse for format yyyymmddhh_mm_ss'],
-['-b', '--base-url', '<url>', 'Set the base url in the item link property of the output RSS file'],
 ['-B', '--base-directory', '<url>', 'Set the base directory to output the RSS file and the MPEG4 files'],
 ['-d', '--rss-description', '<description>', 'Set the description tag in the output RSS file'],
-['-g', '--rss-language', '<language>', 'Set the language tag in the output RSS file'],
 ['-h', '--help', '', 'This is help text.'],
 ['-l', '--rss-link', '<link>', 'Set the link tag in the output RSS file'],
-['-r', '--requested-count', '<url>', 'Set the max request count to the media server contents'],
+['-r', '--requested-count', '<number>', 'Set the max request count to the media server contents'],
 ['-t', '--rss-title', '<file>', 'Set the title tag in the output RSS file'],
-['-f', '--mp4-format', '<ipod | psp>', 'Set the MPEG4 format'],
+['-f', '--rss-file', '<file>', 'RSS filename'],
 ['-s', '--search-title', '<regular expression>', 'Set the regular expression of the content titles by UTF-8'],
 );
 
@@ -108,14 +103,10 @@ for ($i=0; $i<(@ARGV); $i++) {
 		exit 1;
 	} elsif ($opt_short_name eq '-a') {
 		$search_date = $ARGV[++$i];
-	} elsif ($opt_short_name eq '-b') {
-		$rss_base_url = $ARGV[++$i];
 	} elsif ($opt_short_name eq '-B') {
 		$base_directory = $ARGV[++$i];
 	} elsif ($opt_short_name eq '-d') {
 		$rss_description = $ARGV[++$i];
-	} elsif ($opt_short_name eq '-g') {
-		$rss_language = $ARGV[++$i];
 	} elsif ($opt_short_name eq '-l') {
 		$rss_link = $ARGV[++$i];
 	} elsif ($opt_short_name eq '-r') {
@@ -123,11 +114,7 @@ for ($i=0; $i<(@ARGV); $i++) {
 	} elsif ($opt_short_name eq '-t') {
 		$rss_title = $ARGV[++$i];
 	} elsif ($opt_short_name eq '-f') {
-		$mp4_format = $ARGV[++$i];
-		if ($mp4_format ne 'ipod' && $mp4_format ne 'psp') {
-			print "Unkown MPEG4 format : $mp4_format !!\n";
-			exit 1;
-		}
+		$rss_file = $ARGV[++$i];
 	} elsif ($opt_short_name eq '-s') {
 		$title_regexp = $ARGV[++$i];
 	} else {
@@ -135,21 +122,13 @@ for ($i=0; $i<(@ARGV); $i++) {
 	}
 }
 
-if (length($rss_file_name) <= 0) {
-	print "$script_name : Must specify a output RSS file name\n";
-	print "$script_name : try \'$script_name --help\' for more information	\n";
-	exit 1	;
-}
-
-print "$program_name (v$script_version), $copy_right\n";
-print "Output RSS file name = $rss_file_name\n";
+print "  $program_name (v$script_version), $copy_right\n";
+print "  Output RSS file name = $rss_filename\n";
 print "  title : $rss_title\n";
 print "  description : $rss_description\n";
-print "  language : $rss_language\n";
-print "  base url : $rss_base_url\n";
 print "  base directory : $base_directory\n";
 print "  requested_count : $requested_count\n";
-print "  mp4_format : $mp4_format\n";
+print "  rss file : $rss_file\n";
 print "  search regexp : $title_regexp\n";
 
 #------------------------------
@@ -174,7 +153,6 @@ $devNum= 0;
 foreach $dev (@dev_list) {
 	$device_type = $dev->getdevicetype();
 	print "$device_type \n";
-	#print Dumper $dev;
 	#print "Device type is $device_type \n";
 	#print Dumper $dev->getservicebyname('urn:schemas-upnp-org:service:ContentDirectory:1');
 	#print "\n\n\n\n";
@@ -191,7 +169,6 @@ foreach $dev (@dev_list) {
 	#@content_list = $mediaServer->getcontentlist(ObjectID => 0, RequestedCount => $requested_count);
 	@content_list = $mediaServer->getcontentlist(ObjectID => 0);
 	#print "content_list = @content_list\n";
-	#print Dumper @content_list;
 	foreach $content (@content_list) {
 		print "content $content->{_title} \n";
 		if ( $content->{_title} eq 'Video' ) {
@@ -211,7 +188,7 @@ if (@dms_content_list <= 0) {
 	exit 1;
 }
 
-$output_rss_filename = $base_directory . $rss_file_name;
+$output_rss_filename = $base_directory . $rss_file;
 
 open(RSS_FILE, ">$output_rss_filename") || die "Couldn't open the specifed output file($output_rss_filename)\n";
 
@@ -220,7 +197,6 @@ $rss_header = <<"RSS_HEADER";
 <rss xmlns:itunes="http://www.itunes.com/DTDs/Podcast-1.0.dtd" version="2.0">
 <channel>
 <title>$rss_title</title>
-<language>$rss_language</language>
 <description>$rss_description</description>
 <link>$rss_link</link>
 RSS_HEADER
@@ -231,7 +207,8 @@ foreach $content (@dms_content_list){
 	$fname = $content->{'file_name'};
 	$fsize = $content->{'file_size'};
 
-$mp4_link = $rss_base_url . $fname;
+#$mp4_link = $rss_base_url . $fname;
+$mp4_link = $fname;
 $mp4_item = <<"RSS_MP4_ITEM";
 <item>
 <title>$title</title>
@@ -265,9 +242,10 @@ sub parse_content_directory {
 	if ($content->isitem()) {
 		#my $title = $content->gettitle();
 		my $title = $content->{_title};
-		$title =~ tr/a-zA-Z0-9/_/c;
-		my $date = ParseDate($content->{_date});
-		$date =~ tr/a-zA-Z0-9/_/c;
+		$title =~ tr/a-zA-Z0-9/_/cd;
+		#my $date = ParseDate($content->{_date});
+		my $date = $content->{_date};
+		$date =~ tr/0-9//cd;
 		print "title: $title \n";
 		my $mime = $content->{_contenttype};
 		print "mime: $mime \n";
@@ -276,7 +254,7 @@ sub parse_content_directory {
 			if ((length($search_date) == 0) || ($date =~ m/$search_date/)) {
 				my $dms_content_count = @dms_content_list;
 				if ($requested_count == 0 || $dms_content_count < $requested_count) {
-					my $mp4_content = mpeg2tompeg4($mediaServer, $content);
+					my $mp4_content = get_content($mediaServer, $content);
 					if (defined($mp4_content)) {
 						push(@dms_content_list, $mp4_content);
 					}
@@ -290,7 +268,6 @@ sub parse_content_directory {
 	}
 
 	my @child_content_list = $mediaServer->getcontentlist(ObjectID => $objid );
-	#print Dumper @child_content_list;
 	
 	if (@child_content_list <= 0) {
 		return;
@@ -302,26 +279,27 @@ sub parse_content_directory {
 }
 
 #------------------------------
-# mpeg2tompeg4
+# get_content
 #------------------------------
 
-sub mpeg2tompeg4 {
+sub get_content {
 	($mediaServer, $content) = @_;
 	#print Dumper $content;
 	my $objid = $content->getid();
 	my $title = $content->gettitle();
 	$title =~ tr/a-zA-Z0-9/_/c;
 	#my @date = parse_date($content->{_date});
-	my $date = ParseDate($content->{_date});
+	#my $date = ParseDate($content->{_date});
+	my $date = $content->{_date};
 	$date =~ tr/a-zA-Z0-9/_/c;
 
 	my $url = $content->geturl();
 	
-	print "[$objid] $title date: $date ($url)\n";
+	#print "[$objid] $title date: $date ($url)\n";
 	
 	my $dev = $mediaServer->getdevice();
 	my $dev_friendlyname = $dev->getfriendlyname();
-	print "dev_friendly_name $dev_friendlyname\n";
+	#print "dev_friendly_name $dev_friendlyname\n";
 	my $dev_udn = $dev->getudn();
 	$dev_udn =~ s/:/-/g;
 	
@@ -330,43 +308,39 @@ sub mpeg2tompeg4 {
 	$filename_body =~ s/ //g;
 	$filename_body =~ s/\//-/g;
 	
-	my $mpeg2_file_name = $filename_body . ".mpeg";
-	my $mpeg4_file_name = $filename_body . "_" . $mp4_format . ".m4v";
-	my $output_mpeg4_file_name = $base_directory . $mpeg4_file_name;
+	my $raw_file_name = $filename_body . ".mpeg.tmp";
+	my $post_file_name = $filename_body . ".mpeg";
+	my $output_file_name = $base_directory . $post_file_name;
 
-	if (!(-e $output_mpeg4_file_name)) {	
-		$curl_opt = "\"$url\" -o \"$mpeg2_file_name\"";
+	if (!(-e $output_file_name)) {	
+		$curl_opt = "\"$url\" -o \"$raw_file_name\"";
 		print "curl $curl_opt\n";
 		curl($curl_opt);
 
-		if ($mp4_format eq 'psp') {	
-			$ffmpeg_opt = "-y -i \"$mpeg2_file_name\" -bitexact -fixaspect -s 320x240 -r 29.97 -b 768 -ar 24000 -ab 32 -f psp \"$output_mpeg4_file_name\"";
-		}
-		else {
-			$ffmpeg_opt = "-y -i \"$mpeg2_file_name\" -bitexact -fixaspect -s 320x240 -r 29.97 -b 850 -acodec aac -ac 2 -ar 44100 -ab 64 -f mp4 \"$output_mpeg4_file_name\"";
-		}
+		$ffmpeg_opt = "-y -i \"$raw_file_name\" -acodec copy -vcodec copy \"$output_file_name\"";
 	
-		#print "ffmpeg $ffmpeg_opt\n";
-		#ffmpeg($ffmpeg_opt);
+		print "ffmpeg $ffmpeg_opt\n";
+		ffmpeg($ffmpeg_opt);
 		
-		#unlink($mpeg2_file_name);
+		unlink($raw_file_name);
 	}
 		
-	if (!(-e $output_mpeg4_file_name)) {	
+	if (!(-e $output_file_name)) {	
 		return undef;
 	}
 	
-	my $mpeg4_file_size = -s $output_mpeg4_file_name;
+	my $post_file_size = -s $output_file_name;
 	
-	if ($mpeg4_file_size <= 0) {
+	if ($post_file_size <= 0) {
 		return undef;
 	}
 		
 	my %info = (
 		'objid' => $objid,
 		'title' => $title,
-		'file_name' => $mpeg4_file_name,
-		'file_size' => $mpeg4_file_size,
+		'file_name' => $post_file_name,
+		'file_size' => $post_file_size,
+		'file_url'  => $url,
 	);
 	
 	return \%info;
